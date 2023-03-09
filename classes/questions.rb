6aa262dcc30b333c0d0d41f10d72848656eb5793
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-03-09 12:16:12 -0700

    create player and squestion classes

---
+++ classes/questions.rb
@@ -0,0 +1,14 @@
+class Question
+
+    attr_reader :answer
+
+    def initialize 
+        @first_num = rand(1..20)
+        @second_num = rand(1..20)
+        @answer = @first_num + @second_num
+    end
+    
+    def question_string
+        "What does #{@first_num} plus #{@second_num} equal?"
+    end
+end
